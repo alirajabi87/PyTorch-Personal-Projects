b85f2d72ce781899625413316f15ab7f74486459
Ali Rajabi <rajabi.ali.87@gmail.com>
2021-04-03 22:34:29 +0200

    Edided model loading

--- BinaryImagClassification/DeployingTheModel.py
+++ BinaryImagClassification/DeployingTheModel.py
@@ -1,23 +1,20 @@
-import torch
+
 from torchvision.utils import make_grid
 import torchvision.transforms as transforms
-from torchsummary import summary
 from BinaryImagClassification.classes.Model import Net
 from BinaryImagClassification.classes.utils import *
 from BinaryImagClassification.classes.dataset import HistoCancerDataset, CheckandDraw
-
 from sklearn.metrics import accuracy_score
 
 import os
 import time
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 def deploy_model(model, dataset, device, num_classes=2, sanity_check=False):
     len_data = len(dataset)
     y_out = torch.zeros(len_data, num_classes)
-    y_gt = np.zeros((len_data), dtype=np.uint8)
+    y_gt = np.zeros((len_data), dtype="uint8")
     model = model.to(device)
     elapsed_times = []
     with torch.no_grad():
@@ -36,14 +33,16 @@ def deploy_model(model, dataset, device, num_classes=2, sanity_check=False):
 
 
 def main():
-    params_model = dict(input_shape=(3, 96, 96), initial_filter=8, num_hidden_fc=100,
+    params_model = dict(input_shape=(3, 96, 96), initial_filter=16, num_hidden_fc=100,
                         num_classes=2, dropout_rate=0.25)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model = Net(params_model)
     # summary(model, params_model["input_shape"], device=device.type)
 
     if os.path.exists("./models/weights.pt"):
+        print("==> loading the model...")
         model.load_state_dict(torch.load("./models/weights.pt"))
+        model.eval()
 
     path = "../Data/histopathologic-cancer/"
     transform = transforms.Compose([transforms.ToTensor()])
@@ -52,7 +51,7 @@ def main():
     data.transform = data.transformType(Datatype="validation")
 
     _, val_data = data.train_test_split(data)
-    data.transform = data.transformType(Datatype="validation")
+    # data.transform = data.transformType(Datatype="validation")
 
     print(len(val_data))
     y_out, y_gt = deploy_model(model, dataset=val_data, device=device, sanity_check=False)
@@ -60,7 +59,7 @@ def main():
 
     y_pred = np.argmax(y_out, axis=1)
     print(y_pred.shape)
-    acc = accuracy_score(y_pred, y_gt)
+    acc = accuracy_score(y_pred=y_pred, y_true=y_gt)
     print(f"accuracy: {acc*100:.2f}%")
 
     grid_size = 4
